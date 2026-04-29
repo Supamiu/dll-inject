5007c290270c6198d978597aa0e7655dd7229d5e
Brandon Birkholz <brandonjbirkholz@gmail.com>
2026-04-29 17:53:48 -0500

    fix: make Windows only

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-module.exports = require("bindings")({
+if (process.platform !== "win32") {
+  module.exports = { getPIDByName: () => -1, injectPID: () => -1 };
+} else {
+  module.exports = require("bindings")({
     bindings: "injector.node",
     module_root: __dirname,
-});
+  });
+}
